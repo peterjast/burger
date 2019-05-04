b0f9380c89409a1f08627f5f085bbca62840ed6e
peterjast <peterjast@yahoo.com>
2019-05-03 19:46:29 -0700

    yee

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 8080;
 
@@ -9,6 +10,7 @@ app.use(express.static("public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
