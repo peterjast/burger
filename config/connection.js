dd008df25ef27db8f9f7412005b86a7e5f8da2b4
peterjast <peterjast@yahoo.com>
2019-05-11 06:43:49 -0700

    hmm

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL || "mysql://svkf1jpttcneo6ck:zc44bs0iyvszjw3a@tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hvtj77q35qt6t65x")
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
