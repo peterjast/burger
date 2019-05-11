5b403f89918098e734c6929120d407e6189d9d9a
peterjast <peterjast@yahoo.com>
2019-05-11 05:54:23 -0700

    changes

---
+++ db/JAWSDB.sql
@@ -0,0 +1,12 @@
+USE sm7l6rrr84zxky7d;
+DROP TABLE IF EXISTS burgers;
+CREATE TABLE burgers(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	eaten BOOLEAN NOT NULL,
+	PRIMARY KEY (id)
+);
+INSERT INTO burgers (name, eaten) VALUES ("Whopper", FALSE);
+INSERT INTO burgers (name, eaten) VALUES ("Krabby Patty", FALSE);
+INSERT INTO burgers (name, eaten) VALUES ("Goodburger", FALSE);
+SELECT * FROM burgers;
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 8080;
-
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
+
+// parse application/json
+app.use(bodyParser.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -24,8 +25,6 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 });
